baf84975ffa87f1498445c035532406b72151af7
NicDunning <72279040+NicDunning@users.noreply.github.com>
2024-12-15 15:49:16 -0500

    Add Hero Section (#5)

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import styles from './App.module.css'
 import {Navbar} from "./components/navbar/Navbar.jsx";
+import {Hero} from "./components/hero/Hero.jsx"
 
 function App() {
 
   return (
-    <div className={styles.App}>
-      <Navbar/>
-    </div>
+      <div className={styles.App}>
+          <Navbar/>
+          <Hero/>
+      </div>
   )
 }
 
---
+++ MyPortfolio/src/components/hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import styles from "./Hero.module.css"
+import {getImageUrl} from "../../utils.js";
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I am Nicolas</h1>
+                <p className={styles.description}>I am a Full-Stack Developer with 2 years of professional experience using React, NodeJS, an Python! Reach out if you would like to learn more!</p>
+                <a className={styles.contactBtn} href="mailto:dunningnicolas@gmail.com">Contact Me</a>
+            </div>
+            <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt={"Hero image of me"}/>
+            <div className={styles.topBlur}></div>
+            <div className={styles.bottomBlur}></div>
+        </section>
+    )
+}
