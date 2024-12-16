a502e8399704b0bb0394454a407645165b6b867a
NicDunning <dunningnicolas@gmail.com>
2024-12-15 19:23:28 -0500

    Add Experiences

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import styles from './App.module.css'
 import {Navbar} from "./components/navbar/Navbar.jsx";
 import {Hero} from "./components/hero/Hero.jsx"
 import {About} from "./components/about/About.jsx";
+import {Experience} from "./components/experience/Experience.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
           <Navbar/>
           <Hero/>
           <About/>
+          <Experience/>
       </div>
   )
 }
