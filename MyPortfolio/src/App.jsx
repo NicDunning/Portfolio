63fa7ac4cd6cc6c4a4b7bca9e1c5788dfa2256ff
NicDunning <dunningnicolas@gmail.com>
2024-12-15 22:42:52 -0500

    Add Projects and ProjectCards

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -3,6 +3,7 @@ import {Navbar} from "./components/navbar/Navbar.jsx";
 import {Hero} from "./components/hero/Hero.jsx"
 import {About} from "./components/about/About.jsx";
 import {Experience} from "./components/experience/Experience.jsx";
+import {Projects} from "./components/projects/Projects.jsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Hero/>
           <About/>
           <Experience/>
+          <Projects/>
       </div>
   )
 }
