19222450a3c0c9e0c09f8f6720741ee0faa707d5
NicDunning <72279040+NicDunning@users.noreply.github.com>
2025-01-21 16:23:32 -0500

    Update hero image (#18)
    * Add hero image
    
    * Clean Text

--- MyPortfolio/src/components/hero/Hero.jsx
+++ MyPortfolio/src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I am Nicolas</h1>
-                <p className={styles.description}>I am a Full-Stack Developer with 2 years of professional experience using React, NodeJS, an Python! Reach out if you would like to learn more!</p>
+                <p className={styles.description}>A Full-Stack Developer with 2 years of professional experience using React, NodeJS, and Python! Reach out if you would like to learn more!</p>
                 <a className={styles.contactBtn} href="mailto:dunningnicolas@gmail.com">Contact Me</a>
             </div>
             <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt={"Hero image of me"}/>
