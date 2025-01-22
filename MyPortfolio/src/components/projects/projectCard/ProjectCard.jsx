4d2e7e4bc0833a2082ac8613e906d90982fc2f9b
NicDunning <dunningnicolas@gmail.com>
2025-01-22 09:27:43 -0500

    Replace sample data

--- MyPortfolio/src/components/projects/projectCard/ProjectCard.jsx
+++ MyPortfolio/src/components/projects/projectCard/ProjectCard.jsx
@@ -1,15 +1,53 @@
-import React from "react"
+import {React, useState, useCallback} from "react"
+import ImageViewer from 'react-simple-image-viewer';
 import PropTypes from "prop-types";
 
 import {getImageUrl} from "../../../utils.js";
 import styles from "./ProjectCard.module.css";
 
-export const ProjectCard = ({project : {title, description, imageSrc, skills, demo, source}}) => {
+export const ProjectCard = ({project : {title, description, imageSrc, skills, demo, demoEnabled, source, sourceEnabled}}) => {
+
+    const [activeImage, setActiveImage] = useState(0)
+    const [isViewerOpen, setIsViewerOpen] = useState(false)
+
+    const openImageViewer = useCallback((index) => {
+        setActiveImage(index);
+        setIsViewerOpen(true);
+    }, []);
+
+    const closeImageViewer = () => {
+        setActiveImage(0);
+        setIsViewerOpen(false);
+    };
+
+    const imageSrcs = imageSrc.map((img) => getImageUrl(img))
+
     return (
-        <div className={styles.container}>
-            <img className={styles.image} src={getImageUrl(imageSrc)} alt={`Image of ${title}`}/>
+        <div className={styles.container} title={description}>
+            <div className={styles.slideshowContainer}>
+
+                <img className={styles.image}
+                     onClick={openImageViewer}
+                     src={imageSrcs[0]} alt={`Image of ${title}`}/>
+
+                {isViewerOpen && (
+                    <ImageViewer
+                      src={imageSrcs}
+                      currentIndex={ 0 }
+                      disableScroll={ false }
+                      closeOnClickOutside={ true }
+                      onClose={ closeImageViewer }
+                    />
+                )}
+                {/*<a className={styles.prev} onClick={() => {*/}
+                {/*    setActiveImage(Math.max(0, activeImage - 1))*/}
+                {/*}}>&#10094;</a>*/}
+                {/*<a className={styles.next} onClick={() => {*/}
+                {/*    setActiveImage(Math.min(imageSrc.length - 1, activeImage + 1))*/}
+                {/*}}>&#10095;</a>*/}
+            </div>
             <h3 className={styles.title}>{title}</h3>
-            <p className={styles.description}>{description}</p>
+            {/*<p className={styles.description}>{description}</p>*/}
             <ul className={styles.skills}>
                 {
                     skills.map((skill, skillId) => {
@@ -20,8 +58,10 @@ export const ProjectCard = ({project : {title, description, imageSrc, skills, de
                 }
             </ul>
             <div className={styles.links}>
-                <a className={styles.link} href={demo}>Demo</a>
-                <a className={styles.link} href={source}>Source</a>
+                <a className={demoEnabled ? styles.link : `${styles.disabled}`}
+                   href={demoEnabled ? demo : "javascript:void(0)"}>Demo</a>
+                <a className={sourceEnabled ? styles.link : `${styles.disabled}`}
+                   href={sourceEnabled ? source : "javascript:void(0)"}>Source</a>
             </div>
         </div>
     )
@@ -34,6 +74,8 @@ ProjectCard.propTypes = {
         imageSrc: PropTypes.string,
         skills: PropTypes.array,
         demo: PropTypes.string,
-        source: PropTypes.string
+        demoEnabled: PropTypes.bool,
+        source: PropTypes.string,
+        sourceEnabled: PropTypes.bool,
     }
 }
